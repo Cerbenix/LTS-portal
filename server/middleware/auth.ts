26b63b052e757b98aedab4f6c664b89a7aff4d41
Matīss <kmaters@gmail.com>
2026-08-18 23:12:05 +0300

    Fixed middleware preventing hitting the webhook, moved webhook endpoint to routes

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,5 @@
 export default defineEventHandler(async (event) => {
   if (event.node.req.url?.startsWith('/api/')) {
-    if (event.node.req.url.startsWith('/api/webhook/')) return
     await requireUserSession(event)
   }
 })
